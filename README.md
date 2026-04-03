9c06b4ad8aadf31667d208b670a74c0b97d9a7d4
Hostilejoe2 <220639277+HostileJoe2@users.noreply.github.com>
2026-04-03 18:28:20 -0500

    I finished the rock paper scissors project in about an hour. I have the logic figured out, but syntax is still a challenge for me.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # rock-paper-scissors
+
+This is my attempt at making a rock paper scissors game using JS. 
+
+We will see how long it takes me to do it.
+
+
+okay. So this took quite a bit of research with Gemini. I evidently am STILL struggling with syntax and logical order of operations. I'm not sure if I'm overthinking things or underthinking things..
+
+I've got the logic down, and how functions are supposed to work. defining a function to expect parameters was partly what I was struggling with, and once I understood that this project got much easier. 
+
+I know the course says don't use AI to complete your code for you. However, I don't think that I'm doing this course wrong in using Gemini as a tutor. I don't really have anyone else to ask questions to, and sometimes the lessons and the reading don't make sense to me at all. 
+
+Oh well. Onward and upwards!
---
+++ rock-paper-scissors.js
@@ -0,0 +1,59 @@
+
+let computerChoice = getComputerChoice()
+let humanChoice = getHumanChoice()
+let humanScore = 0
+let computerScore = 0
+
+
+playRound(computerChoice, humanChoice);
+
+console.log(humanChoice)
+console.log("You: " + humanScore);
+console.log("Computer: " + computerScore);
+
+//the computer gets it's choice from a random number between 1 and 100
+function getComputerChoice () {
+
+    let num = Math.floor(Math.random() * 100)
+    let computerChoice = "damnit"
+
+    if (num <= 33) {
+        computerChoice = "Rock"
+    } else if (num <= 66) {
+        computerChoice = "Paper"
+    } else {
+        computerChoice = "Scissors"
+    }
+    return computerChoice;    
+}
+
+//promtps the player on page load to choose between the three
+function getHumanChoice () {
+    let humanChoice = prompt("Pick Rock, Paper, or Scissors")
+    return humanChoice;
+}
+
+//this is the logic for how games are scored
+function playRound (computerChoice, humanChoice) {
+    mine = computerChoice.toLowerCase();
+    theirs = humanChoice.toLowerCase();
+
+    if (mine === 'rock' && theirs === 'scissors') {
+        console.log("You win! Rock Beats Scissors")
+        humanScore += 1
+    } else if (mine === 'scissors' && theirs === 'paper') {
+        console.log("You Win! Scissors beats Paper!")
+        humanScore += 1
+    } else if (mine === 'paper' && theirs == 'rock') {
+        console.log("You Win! Paper beats Rock!")
+        humanScore += 1
+    } else if (mine === theirs) {
+        console.log ("It's a draw!")
+    } else {
+        console.log("You Loose! " + theirs +"beats " + mine)
+        computerScore += 1
+    }
+}
+
+
+
